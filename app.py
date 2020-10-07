5c468af7d68ea28e7ee439d7a3348a7d50d7b2e7
Sumit Shukla <45198578+ssumitshukla@users.noreply.github.com>
2020-10-08 03:54:46 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,12 @@
 
 from flask import *
 import pickle
+import gspread
+
+gc = gspread.service_account(filename = "cred.json")
+sh = gc.open_by_key("10Eaw0vi941eOeapiAVvfScrEySvWZBQiP_DmyPTUEfI")
+
+worksheet = sh.Sheet1
 
 app = Flask(__name__, static_url_path='/static')
 model = pickle.load(open('random_forest_titanic_model.pkl', 'rb'))
@@ -11,6 +17,7 @@ def Home():
 @app.route("/add", methods = ['POST'])
 def predict():
 	if request.method == 'POST':
+		name = request.form['fn']
 		pclass = int(request.form['pclass'])
 		gender = int(request.form['sex'])
 		age = float(request.form['age'])
@@ -28,6 +35,8 @@ def predict():
 		out = "Error"
 		if pred ==1:out = "Survived"
 		else: out = "Didn't Survived"
+			
+		worksheet.append_row([name, out])
 		return render_template('index.html', results = out)
 	else:
 		return render_template('index.html')
